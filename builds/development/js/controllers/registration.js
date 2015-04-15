8e6a4052e3610af2b8c04069fe45d386c44ce463
Sarthak Singh <sarthak91singh@gmail.com>
2015-04-16 01:04:37 +0530

    25% course completed

---
+++ builds/development/js/controllers/registration.js
@@ -0,0 +1,12 @@
+myApp.controller('RegistrationController',function($scope, $location){
+
+	$scope.login = function(){
+		$location.path('/meetings');
+	}
+
+	$scope.register = function(){
+		$location.path('/meetings');
+	}
+
+	
+});
