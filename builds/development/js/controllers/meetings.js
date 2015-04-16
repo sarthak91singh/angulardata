219aa7b682797f86ea2ba4579c2c9f2f95e85ba2
Sarthak Singh <sarthak91singh@gmail.com>
2015-04-16 16:51:23 +0530

    Office task

--- builds/development/js/controllers/meetings.js
+++ builds/development/js/controllers/meetings.js
@@ -3,6 +3,21 @@ myApp.controller('MeetingsController',function($scope,$firebaseObject){
 	var ref = new Firebase("https://sarthakangularapp.firebaseio.com/meetings");
 
 	$scope.meetings = $firebaseObject(ref);
-	
+
+	var meetings = $scope.meetings;
+
+	$scope.addMeeting = function(){
+		console.log("Inside addMeeting function.....");
+		ref.push({
+			name: $scope.meetingname,
+			date: "29 Feb"
+		});
+	};
+
+	$scope.deleteMeeting = function(key){
+		var newRef = ref.child(key);
+		console.log(newRef);
+		newRef.remove();
+	};
 
 });
--- builds/development/js/controllers/registration.js
+++ builds/development/js/controllers/registration.js
@@ -1,12 +1,27 @@
-myApp.controller('RegistrationController',function($scope, $location){
+myApp.controller('RegistrationController',function($scope, $location, $firebaseAuth){
+
+	console.log("Inside RegistrationController");
+	var ref = new Firebase("https://sarthakangularapp.firebaseio.com/meetings");
+
+	var auth = $firebaseAuth(ref);
 
 	$scope.login = function(){
-		$location.path('/meetings');
+
+		console.log("auth is:",auth);
+
+		auth.$authWithPassword({
+			email: $scope.user.email,
+			password: $scope.user.password
+		}).then(function(){
+			$location.path('/meetings');
+		}).catch(function(error){
+			$scope.message = error.message;
+		});
 	}
 
 	$scope.register = function(){
 		$location.path('/meetings');
 	}
 
-	
+
 });
