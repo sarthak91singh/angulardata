9c288e5b0cf44eaad147c5d54e282d3106750031
Sarthak Singh <sarthak91singh@gmail.com>
2015-05-27 09:03:24 +0530

    Hii

--- builds/development/js/controllers/meetings.js
+++ builds/development/js/controllers/meetings.js
@@ -1,31 +1,49 @@
 myApp.controller('MeetingsController',function($scope,$firebaseObject, $firebaseArray, FIREBASE_URL){
+
 	console.log("Inside MeetingsController");
-	var ref = new Firebase("https://sarthakangularapp.firebaseio.com/meetings");
+	var regUserGlobal;
+	var meetingRef;
 
-	var meetings = $firebaseObject(ref);
-	$scope.meetings = meetings;
 
-	var meetingsArray = $firebaseArray(ref);
 
-	meetingsArray.$loaded(function(data){		
-		$scope.meetingsLength = meetingsArray.length;
-		console.log("LENGTH:",meetingsArray.length);
-	});
 
-	meetingsArray.$watch(function(data){
-		$scope.meetingsLength = meetingsArray.length;		
-	});
+	// Register the callback to be fired every time auth state changes
+	var ref = new Firebase("https://sarthakangularapp.firebaseio.com");
+	ref.onAuth(authDataCallback);
+
+	// Create a callback which logs the current auth state
+	function authDataCallback(authData) {
+	  if (authData) {
+	  	$scope.currentUser.regUser = authData.uid;
+	  	regUserGlobal = authData.uid
+	  	console.log("AUTHDATA IS:", authData);
+	    console.log("User " + authData.uid + " is logged in with " + authData.provider);
+	  } else {
+	    console.log("User is logged out");
+	  }
+
+	  meetingRef = new Firebase(FIREBASE_URL + "/users/" + regUserGlobal + "/meetings");
+
+	}
+
+
+
 
 	$scope.addMeeting = function(){
 		console.log("Inside addMeeting function.....");
-		ref.push({
+		console.log("Global USER ID",regUserGlobal);
+		meetingRef.push({
 			name: $scope.meetingname,
 			date: "29 Feb"
 		});
 	};
 
+
+
+
+
 	$scope.deleteMeeting = function(key){
-		var newRef = ref.child(key);
+		var newRef = meetingRef.child(key);
 		console.log(newRef);
 		newRef.remove();
 	};
@@ -33,20 +51,33 @@ myApp.controller('MeetingsController',function($scope,$firebaseObject, $firebase
 
 
 
-// Create a callback which logs the current auth state
-function authDataCallback(authData) {
-  if (authData) {
-  	$scope.currentUser.regUser = authData.uid;
-  	console.log("AUTHDATA IS:", authData);
-    console.log("User " + authData.uid + " is logged in with " + authData.provider);
-  } else {
-    console.log("User is logged out");
-  }
-}
 
-// Register the callback to be fired every time auth state changes
-var ref = new Firebase("https://sarthakangularapp.firebaseio.com");
-ref.onAuth(authDataCallback);
+
+
+	var myref = new Firebase(FIREBASE_URL + "/meetings");
+	var meetings = $firebaseObject(meetingRef);
+	$scope.meetings = meetings;
+
+	var meetingsArray = $firebaseArray(meetingRef);
+
+	console.log("Meetings array is:", meetingsArray);
+
+
+
+
+	meetingsArray.$loaded(function(data){
+		$scope.meetingsLength = meetingsArray.length;
+		console.log("LENGTH:",meetingsArray.length);
+	});
+
+
+
+
+
+
+	meetingsArray.$watch(function(data){
+		$scope.meetingsLength = meetingsArray.length;
+	});
 
 
 });
